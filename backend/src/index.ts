134a60e59f003fcf6286b13b0e19bffb953eaa8e
chakripavankumar <siriyalapavankumar@gmail.com>
2024-05-16 13:00:00 +0530

    routing✅

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,13 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
 
 
+
 const app = new Hono<{
   Bindings:{
     DATABASE_URL:string;
     JWT_SECRET_TOKEN:string;
   }
 }>()
-
 
 app.post('/api/v1/user/signup' , async (c)=>{
   const body= await c.req.json();
@@ -19,7 +19,7 @@ app.post('/api/v1/user/signup' , async (c)=>{
   }).$extends(withAccelerate())
 
 try{
- const user= prisma.user.create({
+ const user=  await prisma.user.create({
     data:{
       username:body.username,
       password:body.password,
@@ -27,19 +27,45 @@ try{
     }
   })
   const  jwt= await sign({
-    id: (await user).id
+    id: user.id
   },c.env.JWT_SECRET_TOKEN)
-
   return  c.text(jwt)
 } catch(e){
   c.status(411);
  return c.text('invalid')
 }
+})
 
+app.post('/api/v1/user/signin' ,async (c)=>{
+  const body= await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+try{
+ const user=  await prisma.user.findFirst({
+    where:{
+      username:body.username,
+      password:body.password,
+      name:body.name
+    }
+  })
+  if(!user){
+    c.status(403)
+    return c.text("check your creedtials")
+  }
+  const  jwt= await sign({
+    id: user.id
+  },c.env.JWT_SECRET_TOKEN)
+
+  return  c.text(jwt)
+}
+catch(e){
+  c.status(411);
+ return c.text('invalid')
+}
 })
-app.post('/api/v1/user/signin' , (c)=>{
-  return c.text('Hello world')
-})
+
 app.post('/api/v1/blog' , (c)=>{
   return c.text('Hello world')
 })
