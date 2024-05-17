363e7017796aa9d0fae999841c25cc6a815db3c8
chakripavankumar <siriyalapavankumar@gmail.com>
2024-05-17 21:47:26 +0530

    zod-validation✅

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,9 +1,21 @@
  
- import z from 'zod';
+ import z, { string } from 'zod';
  
  export const signedUpInput= z.object({
     username:z.string().email(),
     password:z.string().min(6),
     name:z.string().optional()
   })
-  
+  export const signedinInput= z.object({
+    username:z.string().email(),
+    password:z.string().min(6),
+  })
+   export const createBlogInput= z.object({
+       title:z.string(),
+       content: string(),
+       id:z.number()
+   })
+   export type SignedUpInput= z.infer<typeof signedUpInput>
+   export type CreateBlogInput=z.infer<typeof createBlogInput>
+   export type SignedinInput= z.infer<typeof signedinInput>
+ 
